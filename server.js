d28f2aca883ea025589ffdbe44ef2e4491c6f542
delixa1243 <felixarajiph@gmail.com>
2022-03-17 14:31:07 +0700

    pug engine v 1.0

--- server.js
+++ server.js
@@ -8,11 +8,19 @@ const port=3000
 //middleware
 
 //engine
-app.set('views',path.join(__dirname+'views'))
-app.set('view engine',pug)
+app.set('views',path.join(__dirname+'/views'))
+app.set('view engine','pug')
 //route
 app.get('/',(req,res)=>{
-res.render('index')})
+	res.render('index',{
+		title:"root",content:"roor content"
+	})
+})
+app.get('/home',(req,res)=>{
+	res.render('home',{
+		title:"home",content:"home content"
+	})
+})
 //listen
 app.listen(port,()=>{
 console.log(`App running on port ${port}`)})
