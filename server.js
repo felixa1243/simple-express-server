5b737a6d7b0d0148dfef06040b854a97e71ef312
delixa1243 <felixarajiph@gmail.com>
2022-03-18 00:25:33 +0700

    init for update model

--- server.js
+++ server.js
@@ -12,13 +12,30 @@ app.set('views',path.join(__dirname+'/views'))
 app.set('view engine','pug')
 //route
 app.get('/',(req,res)=>{
+let articles=[
+	{
+	  titles:"Titles1",
+          articles:"Articles1"
+	},
+	{
+	  titles:"Titles2",
+	  articles:"Articles2"
+	},
+	{
+	  titles:"Titles3",
+	  articles:"Articles3"
+	}
+]
 	res.render('index',{
-		title:"root",content:"roor content"
+		title:"root",
+		content:"root content",
+		articles:articles
 	})
 })
 app.get('/home',(req,res)=>{
 	res.render('home',{
-		title:"home",content:"home content"
+		title:"home",
+		content:"home content"
 	})
 })
 //listen
